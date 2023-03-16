625857b3b8357c53fc65e74181c629d57c41106a
AmandaLanducci <heloisapt_gpsbr@hotmail.com>
2023-03-15 22:38:12 -0300

    atualizações

---
+++ app/Models/Game.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $table = "games";
+
+    protected $fillable = [
+        'genero',
+        'nome',
+        'valor',
+        'descrição',
+        'ano',
+        'estudio'
+
+
+    ];
+}
+
